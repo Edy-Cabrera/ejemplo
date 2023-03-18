7bf973318ed22a29bcd46016d5a4389e3dc991fe
Edyc-Cristianm <127272345+Edyc-Cristianm@users.noreply.github.com>
2023-03-17 21:23:24 -0600

    ejemplo mdo

--- src/Clases/Huesped.java
+++ src/Clases/Huesped.java
@@ -11,6 +11,16 @@ public class Huesped implements Serializable{
     private String Correo;
     private String Telefono;
     private String Nit;
+private String Nit2;
+
+    public String getNit2() {
+        return Nit2;
+    }
+
+    public void setNit2(String Nit2) {
+        this.Nit2 = Nit2;
+    }
+
 
     public void setCodigo(String Codigo) {
         this.Codigo = Codigo;
